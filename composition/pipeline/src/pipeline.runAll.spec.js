fd27afc0ee4428ddf5be18606e9d1612c1ad4eb7
John F Kidd <j.kidd@CL-269L.local>
2017-03-02 17:37:10 +0000

    Renamed from workflow to pipeline
    The name workflow was a confusing name as it has too many connotations.
    We have decided on pipeline as this is a more applicable name.

--- composition/pipeline/src/pipeline.runAll.spec.js
+++ composition/pipeline/src/pipeline.runAll.spec.js
@@ -1,9 +1,11 @@
 import expect from 'expect';
 import { Result } from "./result";
-import { runUntilFirstFault } from "./workflow";
+import { runAll } from "./pipeline";
 
 const INVALID_NAME = "invalid name";
 const INVALID_EMAIL_ADDRESS = "invalid email address";
+const VALID_NAME = "John";
+const VALID_EMAIL_ADDRESS = "jk@test.com";
 
 function validateName(data) {
     if (data === null || !data.name || data.name === null)
@@ -22,14 +24,14 @@ function updateEmailAddress(data) {
     return new Result("", newInstanceWithDifferentEmailAddress);
 }
 
-describe("runUntilFirstFault", () => {
+describe("runAll", () => {
     it("should return that we have no errors", () => {
         // arrange
         const data = { name: "John F Kidd", emailAddress: "john@test.com" };
         const expected = "";
 
         // act
-        const result = runUntilFirstFault(data, [validateName, validateEmail]);
+        const result = runAll(data, [validateName, validateEmail]);
         const actual = result.getError();
 
         // assert
@@ -42,7 +44,7 @@ describe("runUntilFirstFault", () => {
         const expected = INVALID_NAME;
 
         // act
-        const result = runUntilFirstFault(data, [validateName, validateEmail]);
+        const result = runAll(data, [validateName, validateEmail]);
         const actual = result.getError();
 
         // assert
@@ -55,23 +57,37 @@ describe("runUntilFirstFault", () => {
         const expected = INVALID_EMAIL_ADDRESS;
 
         // act
-        const result = runUntilFirstFault(data, [validateName, validateEmail]);
+        const result = runAll(data, [validateName, validateEmail]);
         const actual = result.getError();
 
         // assert
         expect(expected).toEqual(actual);
     });
 
-    it("should return that we have an invalid name", () => {
+    it("should return that we have an invalid name and an invalid aemail address", () => {
         // arrange
         const data = { name: null, emailAddress: null };
-        const expected = INVALID_NAME;
+        const expected = `${INVALID_NAME}<br/>${INVALID_EMAIL_ADDRESS}`;
 
         // act
-        const result = runUntilFirstFault(data, [validateName, validateEmail]);
+        const result = runAll(data, [validateName, validateEmail]);
         const actual = result.getError();
 
         // assert
+        expect(expected).toEqual(actual);
+    });
+
+    it ("should return new data object with updated email address", () => {
+        // arrange
+        const data = { name: VALID_NAME, emailAddress: INVALID_EMAIL_ADDRESS };
+        const expected = VALID_EMAIL_ADDRESS;
+
+        // act
+        const result = runAll(data, [validateName, validateEmail, updateEmailAddress]);
+        const actual = result.getData().emailAddress;
+
+        // assert
+        expect(data).toNotEqual(result.getData());
         expect(expected).toEqual(actual);
     });
 });
--- composition/pipeline/src/pipeline.runUntilFirstFault.spec.js
+++ composition/pipeline/src/pipeline.runUntilFirstFault.spec.js
@@ -1,11 +1,9 @@
 import expect from 'expect';
 import { Result } from "./result";
-import { runAll } from "./workflow";
+import { runUntilFirstFault } from "./pipeline";
 
 const INVALID_NAME = "invalid name";
 const INVALID_EMAIL_ADDRESS = "invalid email address";
-const VALID_NAME = "John";
-const VALID_EMAIL_ADDRESS = "jk@test.com";
 
 function validateName(data) {
     if (data === null || !data.name || data.name === null)
@@ -24,14 +22,14 @@ function updateEmailAddress(data) {
     return new Result("", newInstanceWithDifferentEmailAddress);
 }
 
-describe("runAll", () => {
+describe("runUntilFirstFault", () => {
     it("should return that we have no errors", () => {
         // arrange
         const data = { name: "John F Kidd", emailAddress: "john@test.com" };
         const expected = "";
 
         // act
-        const result = runAll(data, [validateName, validateEmail]);
+        const result = runUntilFirstFault(data, [validateName, validateEmail]);
         const actual = result.getError();
 
         // assert
@@ -44,7 +42,7 @@ describe("runAll", () => {
         const expected = INVALID_NAME;
 
         // act
-        const result = runAll(data, [validateName, validateEmail]);
+        const result = runUntilFirstFault(data, [validateName, validateEmail]);
         const actual = result.getError();
 
         // assert
@@ -57,37 +55,23 @@ describe("runAll", () => {
         const expected = INVALID_EMAIL_ADDRESS;
 
         // act
-        const result = runAll(data, [validateName, validateEmail]);
+        const result = runUntilFirstFault(data, [validateName, validateEmail]);
         const actual = result.getError();
 
         // assert
         expect(expected).toEqual(actual);
     });
 
-    it("should return that we have an invalid name and an invalid aemail address", () => {
+    it("should return that we have an invalid name", () => {
         // arrange
         const data = { name: null, emailAddress: null };
-        const expected = `${INVALID_NAME}<br/>${INVALID_EMAIL_ADDRESS}`;
+        const expected = INVALID_NAME;
 
         // act
-        const result = runAll(data, [validateName, validateEmail]);
+        const result = runUntilFirstFault(data, [validateName, validateEmail]);
         const actual = result.getError();
 
         // assert
-        expect(expected).toEqual(actual);
-    });
-
-    it ("should return new data object with updated email address", () => {
-        // arrange
-        const data = { name: VALID_NAME, emailAddress: INVALID_EMAIL_ADDRESS };
-        const expected = VALID_EMAIL_ADDRESS;
-
-        // act
-        const result = runAll(data, [validateName, validateEmail, updateEmailAddress]);
-        const actual = result.getData().emailAddress;
-
-        // assert
-        expect(data).toNotEqual(result.getData());
         expect(expected).toEqual(actual);
     });
 });
